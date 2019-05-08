f4ca77334dfab272a2034e92d7a6f82dd907b0d8
Joe Berry <joeb@homebrewagency.com>
2019-05-08 15:24:59 -0700

    Update package.js

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'khamoud:slack-api',
-  version: '0.0.2',
+  name: 'joephuz:slack-api',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'Slack API written for meteor',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/krishamoud/meteor-slack-api.git',
+  git: 'https://github.com/joephuz/meteor-slack-api.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -19,6 +19,6 @@ Package.onUse(function(api) {
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('khamoud:slack-api');
+  api.use('joephuz:slack-api');
   api.addFiles('slack-api-tests.js', ['server']);
 });
