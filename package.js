89c4ce0e0a17e093b9fadbc5304d51c62690c7c3
Jaco Swarts <jaco@vatfree.com>
2025-07-10 22:38:19 +0200

    Update for meteor v3

--- package.js
+++ package.js
@@ -1,24 +1,24 @@
 Package.describe({
-  name: 'vatfree:slack-api',
-  version: '0.0.4',
+  name: "vatfree:slack-api",
+  version: "0.0.4",
   // Brief, one-line summary of the package.
-  summary: 'Slack API written for meteor',
+  summary: "Slack API written for meteor",
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/joephuz/meteor-slack-api.git',
+  git: "https://github.com/joephuz/meteor-slack-api.git",
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: "README.md",
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom(['1.1.0.2', '2.0']);
-  api.addFiles('slack-api.js', ['server']);
-  api.export("SlackAPI", ['server']);
-  api.use('http@~2.0.0', ['server']);
+Package.onUse(function (api) {
+  api.versionsFrom(["3.0"]);
+  api.addFiles("slack-api.js", ["server"]);
+  api.export("SlackAPI", ["server"]);
+  api.use("fetch", ["server"]);
 });
 
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('joephuz:slack-api');
-  api.addFiles('slack-api-tests.js', ['server']);
+Package.onTest(function (api) {
+  api.use("tinytest");
+  api.use("joephuz:slack-api");
+  api.addFiles("slack-api-tests.js", ["server"]);
 });
